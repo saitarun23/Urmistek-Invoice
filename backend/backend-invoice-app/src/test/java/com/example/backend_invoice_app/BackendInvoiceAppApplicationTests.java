44b18891aaedf2e6519b43173a8098974dd1c484
saitarun23 <122889220+saitarun23@users.noreply.github.com>
2026-07-02 18:32:05 +0530

    done

---
+++ backend/backend-invoice-app/src/test/java/com/example/backend_invoice_app/BackendInvoiceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_invoice_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendInvoiceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
