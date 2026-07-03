5d26c4dbe9846aa836466f16ffe0e25b91e334aa
saitarun23 <122889220+saitarun23@users.noreply.github.com>
2026-07-04 00:43:42 +0530

    done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
+import { Routes, Route } from "react-router-dom";
+import InvoiceForm from "./components/InvoiceForm.jsx";
+import PaymentSuccess from "./components/PaymentSuccess.jsx";
+import PaymentFailed from "./components/PaymentFailed.jsx";
 import './App.css'
 
 function App() {
   return (
-     
-    <h2>Urmistek</h2>
-
-  )
+     <Routes>
+      <Route path="/" element={<InvoiceForm />} />
+      <Route path="/payment-success" element={<PaymentSuccess />} />
+      <Route path="/payment-failed" element={<PaymentFailed />} />
+    </Routes>
+  );
 }
 
 export default App
